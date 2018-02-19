bd888e82d309143fe7eb276cf359c832e0d7b185
Tomohiro Matsuo <gacktomo@gmail.com>
2018-02-20 00:48:03 +0900

    beta release

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
 const storage = require('electron-json-storage');
 window.jQuery = window.$ = require(__dirname + '/js/jquery-3.3.1.min.js');
-storage.get('config', function (error, data) {
-  if (error) throw error;
-  if (Object.keys(data).length === 0) {
-    // データがないときの処理
-  } else {
-    console.log(data)
-    var div = document.createElement('div');
-    div.textContent = data.data
-    document.body.appendChild(div);
-    // データがあるときの処理
+
+$(function(){
+  var dt = new Date;
+
+  var reloadlist = function(){
+    $("#title-month").html( dt.getFullYear()+"年"+(dt.getMonth()+1)+"月")
+    $("#timecard-list").html("");
+    storage.get('timedata', function (error, data) {
+      if (error) throw error;
+      if (Object.keys(data).length === 0) data = {};
+      if (!data[dt.getFullYear()]) data[dt.getFullYear()] = {};
+      if (!data[dt.getFullYear()][dt.getMonth()+1]) data[dt.getFullYear()][dt.getMonth()+1] = {};
+      for(let i=1; i<=31; i++){
+        var daydata = data[dt.getFullYear()][dt.getMonth()+1][i] || {"start":"","end":""};
+        $("#timecard-list").append("<tr> <td class='text-center'>"+ i +"</td> <td class='text-center'>"
+        + daydata.start +"</td> <td class='text-center'>"
+        + daydata.end + "</td> </tr>");
+      }
+    });
   }
+
+  $("#prev").click(function(){
+    console.log("prev");
+    dt.setMonth(dt.getMonth() -1);
+    reloadlist();
+  });
+  $("#next").click(function(){
+    console.log("next");
+    dt.setMonth(dt.getMonth() +1);
+    reloadlist();
+  });
+
+  reloadlist();
 });
+
--- main.js
+++ main.js
@@ -1,21 +1,53 @@
 'use strict';
 const {BrowserWindow, app, Menu, Tray} = require("electron");
 const storage = require('electron-json-storage');
+const AutoLaunch = require('auto-launch');
+const os = require('os');
 
-var json = { data: new Date };
-storage.set('config', json, function (error) {
-  if (error) throw error;
-});
+if(os.platform()=="darwin"){
+  var launcher = new AutoLaunch({
+    name: 'AutoTimecard',
+    path: app.getPath('exe').match(/(.*\.app)/)[0]
+  });
 
-// ToDo
-// 自動起動設定
-// http://rhysd.hatenablog.com/entry/2016/12/11/191911
+  launcher.enable();
+  launcher.isEnabled()
+  .then(function(isEnabled){
+    if(isEnabled){ return; }
+    launcher.enable();
+  })
+  .catch(function(err){});
+}
 
-var showList = function () { 
+var refreshTime = function() {
+  storage.get('timedata', function (error, data) {
+    if (error) throw error;
+    var dt = new Date;
+    if (Object.keys(data).length === 0) data = {};
+    if (!data[dt.getFullYear()]) data[dt.getFullYear()] = {};
+    if (!data[dt.getFullYear()][dt.getMonth()+1]) data[dt.getFullYear()][dt.getMonth()+1] = {};
+    if (!data[dt.getFullYear()][dt.getMonth()+1][dt.getDate()]) {
+      data[dt.getFullYear()][dt.getMonth()+1][dt.getDate()] = {
+        "start": ('00'+dt.getHours()).slice(-2)+":"+('00'+dt.getMinutes()).slice(-2),
+        "end":   ('00'+dt.getHours()).slice(-2)+":"+('00'+dt.getMinutes()).slice(-2)
+      };
+    }
+    var datestr = dt.getFullYear()+"/"+(dt.getMonth()+1)+"/"+dt.getDate() +" "+ data[dt.getFullYear()][dt.getMonth()+1][dt.getDate()].end + ":00";
+    var end = new Date(datestr);
+    if( end < dt ){
+      data[dt.getFullYear()][dt.getMonth()+1][dt.getDate()].end = ('00'+dt.getHours()).slice(-2)+":"+('00'+dt.getMinutes()).slice(-2);
+    }
+
+    storage.set('timedata', data, function (error) { if (error) throw error; });
+  });
+}
+
+var showCard = function () { 
   const win = new BrowserWindow({
     "show": false,
-    "width": 300, 
-    "height": 500
+    "width": 335, 
+    "height": 680,
+    "minWidth":335
   });
   win.loadURL('file://' + __dirname + '/index.html')
   win.once('ready-to-show', () => { win.show() })
@@ -24,14 +56,16 @@ var showList = function () {
 let tray = null
 app.on('ready', function() {
   tray = new Tray(__dirname + "/images/IconTemplate.png");
-  showList();
+  refreshTime()
+  setInterval(refreshTime, 5000);
+  // showCard();
   const contextMenu = Menu.buildFromTemplate([
     { 
-      label: "表示", 
-      click: showList()    
+      label: "タイムカードを表示", 
+      click: showCard
     },
     { 
-      label: "終了", 
+      label: "アプリケーションを終了", 
       click: function () { app.quit(); }
     }
   ]);
